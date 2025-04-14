f9f953704a3894624a0f6a4f784aa7d6472153cd
rochmarq <rochellebmarquez@gmail.com>
2025-04-14 15:19:54 -0400

    cleanup

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,10 @@
 async function getRecords(data) {
     const fetchedData = await fetch('data/data.json');
     const result = await fetchedData.json();
+    // check console
     console.log('result: ', result);
 
-    // const demoGrab = document.querySelector('#demo')
-
-    // result.card.forEach(item => {
-    //     // console.log('inside foreach: ', item);
-        
-    // });
+    // put the text in the file
     document.getElementById("unresolved").textContent = result.unresolved;
     document.getElementById("overdue").textContent = result.overdue;
     document.getElementById("open").textContent = result.open;
@@ -16,45 +12,3 @@ async function getRecords(data) {
 }
 
 getRecords();
-
-
-
-
-
-// var obj = JSON.parse(data);
-// document.getElementById("demo").innerHTML = 
-//     obj.cards[1].overdue;
-
-
-// async function getData() {
-//     const response = await fetch('/assets/data/data.json');
-//     const data = await response.json();
-//     console.log(data);
-//     }
-//     getData();
-
-// var obj = JSON.parse(data);
-
-// document.getElementById("demo").innerHTML =
-//     obj.overview[1].overdue;
-
-
-// fetch('data.json')
-//     .then(response => response.json())
-//     .then(data => console.log(data));
-
-
-// import data from './data.json';
-// console.log(data);
-
-
-
-// var text = '{ "employees" : [' +
-// '{ "firstName":"John" , "lastName":"Doe" },' +
-// '{ "firstName":"Anna" , "lastName":"Smith" },' +
-// '{ "firstName":"Peter" , "lastName":"Jones" } ]}';
-
-// var obj = JSON.parse(data);
-
-// document.getElementById("demo").innerHTML =
-//     obj.employees[1].firstName + " " + obj.employees[1].lastName;
